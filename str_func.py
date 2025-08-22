97ac856c1e58b677806695f7b552434cecb47aba
Eugene Evstratev <levipupsik321@gmail.com>
2025-08-22 21:56:26 +0300

    hotfix

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def upper_string(value: str):
+def upper_string(value: str) -> str:
     """docstring1"""
     return value.upper()
 
 
-def upper_first(value: str):
+def upper_first(value: str) -> str:
     """docstring2"""
     return value.capitalize()
